1375c33da434c845e0fa2f27c278e0691bf1b7bc
Huyen Vu <vnhuyen@tma.com.vn>
2023-04-12 17:54:53 +0700

    Update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,39 @@
 class ReviewsController < ApplicationController
+  # before_action :find_book
+  before_action :authorized
+
   def new
     @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.book_id = 1 #@book.id
+    @review.user_id = current_user.__id__
+
+    respond_to do |format|
+      if @review.save
+        notice = "review was successfully created."
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_review
+    @review = review.find(params[:id])
+  end
+
+  def find_book
+    @book = review.find(params[:book_id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def review_params
+    params.require(:review).permit(:comment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "books#landing"
   resources :books
   resources :users
+  resources :reviews
   resources :sessions, only: [:new, :create, :destroy]
   get "signup", to: "users#signup", as: "signup"
   post "register", to: "users#register", as: "register"
